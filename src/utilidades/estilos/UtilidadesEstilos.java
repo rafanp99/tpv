7c4ec7c2d1a176c7b88f19485f0e2996adfa1bcf
rafanp99 <rafanp4eso@gmail.com>
2021-06-03 17:31:07 +0200

    'Arreglado' color botones linux, a falta del hover y de ver en Windows'

--- src/utilidades/estilos/UtilidadesEstilos.java
+++ src/utilidades/estilos/UtilidadesEstilos.java
@@ -9,16 +9,19 @@ public final class UtilidadesEstilos {
     private UtilidadesEstilos(){ }
     public static void botonAzul(JButton boton){
         // TODO En linux no los colorea de ninguna forma
-        boton.setOpaque(true);
-        boton.setBackground(new Color(0,39,96));
         boton.setForeground(Color.white);
+        boton.setOpaque(true);
+        boton.setBorderPainted(false);
+        boton.setBackground(new Color(0,39,96));
         boton.setHorizontalAlignment(SwingConstants.CENTER);
         boton.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-        boton.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,32));
+        boton.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,24));
     }
     public static void botonCerrar(JButton boton){
         // TODO En linux no los colorea de ninguna forma
         boton.setForeground(Color.WHITE);
+        boton.setOpaque(true);
+        boton.setBorderPainted(false);
         boton.setBackground(new Color(230,57,70));
         boton.setHorizontalAlignment(SwingConstants.CENTER);
         boton.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,24));
